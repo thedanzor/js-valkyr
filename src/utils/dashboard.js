ba63bd9b8fcecb20aa7c9e03bd047e90f1c6b064
Scott <scott@crowdynews.com>
2016-09-23 21:23:23 +0200

    foundation for the manager added

--- src/utils/dashboard.js
+++ src/utils/dashboard.js
@@ -2,6 +2,8 @@
  * @module Module application
  */
 
+var config = {};
+
 /**
  * Build a configuration file on all the elements we can manage
  *
@@ -9,9 +11,6 @@
  */
 var check = function (wrapper) {
 	var containers = wrapper.querySelectorAll('[dashboard-wrapper]');
-
-	// Build a runtime configuration file
-	var config = {};
 
 	if (containers && containers.length) {
 		for (var i = 0; i < containers.length; i++) {
@@ -21,10 +20,16 @@ var check = function (wrapper) {
 			config[key] = {};
 
 			if (childElements && childElements.length) {
+
+				config[key].element = childElements[y];
+				config[key].state = 'enabled';
+				config[key].children = {};
+
+				// Build the children objects
 				for (var y = 0; y < childElements.length; y++) {
 					var key2 = childElements[y].getAttribute('dashboard-child');
 
-					config[key][key2] = {
+					config[key].children[key2] = {
 						"state": "enabled",
 						"element": childElements[y]
 					}
@@ -33,11 +38,41 @@ var check = function (wrapper) {
 		}
 	}
 
-	console.log(config);
 	return config;
+};
+
+/**
+ * When the user has changed the dashboard we want to save the state
+ */
+var saveState = function () {
+	// We need the config
+	if (config) {
+
+	}
+};
+
+/**
+ * When the user has finished using the dashboard manager, we should hide the manager
+ */
+var removeManager = function () {
+	// We need the config
+	if (config) {
+
+	}
+};
+
+/**
+ * When the user requests the manager we want to display management options for each object
+ */
+var showManager = function () {
+	// We need the config
+	if (config) {
+
+	}
 };
 
 // Export our interfaces for this utility
 module.exports = {
-	check: check
+	check: check,
+	manager: manager
 };
